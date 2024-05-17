92270ef049467112e970c03e734dc72c7f39787f
KrattsA <good.golub@gmail,com>
2024-05-18 02:17:10 +0300

    Fix reg

--- bot.py
+++ bot.py
@@ -253,7 +253,7 @@ def getAptList(update: Update, context):
 def findPhoneNumbers (update: Update, context):
     user_input = update.message.text 
 
-    phoneNumRegex = re.compile(r"(\+7|8)([\( -]\d{3}[\) -]\d{3}[ -]?\d{2}[ -]?\d{2})|(\+7|8)(\d{3}\d{3}[ -]?\d{2}[ -]?\d{2})")
+    phoneNumRegex = re.compile(r"(\+7|8)([ \( -]\d{3}[ \) -]\d{3}[ -]?\d{2}[ -]?\d{2})|(\+7|8)(\d{3}\d{3}[ -]?\d{2}[ -]?\d{2})|(\+7|8)( \(\d{3}\) \d{3}[ -]?\d{2}[ -]?\d{2})")
 
     phoneNumberList = phoneNumRegex.findall(user_input)
 
